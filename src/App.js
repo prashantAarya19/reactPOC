ecc96ba7ddb963eeef958a02aff8302cf78d409e
prashant <k.prashant093@gmail.com>
2023-01-16 16:30:03 +0530

    react states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('dark'); 
+  
+  const toggleMode = () => {
+    if(mode === 'dark')
+      setMode('light');
+    else
+      setMode('dark');  
+  };
+  
   return (
     <>
-      <Navbar title = "Text Utils" aboutText = "About Us"/>
+      <Navbar title = "Text Utils" aboutText = "About Us" mode = {mode} toggleMode={toggleMode}/>
       <div className="container">
-        <TextForm heading="Enter the Text to analyze below"/>
+        {/* <TextForm heading="Enter the Text to analyze below"/> */}
+        <About />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,21 +2,55 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
   const [text, setText] = useState("Enter text here");  
-  const handleOnClick = () => {
-    // const updatedText = text.toUpperCase();
+  const handletoUpperCase = () => {
     setText(text.toUpperCase());
   }
+
+  const handletoLowerCase = () => {
+    setText(text.toLowerCase());
+  }
+
+  const handleFirstLetterCapital = () => {
+    let tempText = text;
+    let tempTextArray = tempText.split(" ");
+    let formattedText = '';
+    for(let i = 0; i < tempTextArray.length; i++) {
+      formattedText = formattedText+' '+capitlizeFirstLetter(tempTextArray[i]);
+    }
+    setText(formattedText);
+  }
+
+  const capitlizeFirstLetter = (text) => {
+    return text.charAt(0).toUpperCase()+text.slice(1);
+  }
+
+  const handleClearText = () => {
+    setText('');
+  }
+
   const handleOnChange = (event) => {
-    console.log("On change");
     setText(event.target.value);
   }
   return (
-     <div className="my-3">
+     <>
+      <div className="container">
         <h3>{props.heading}</h3>
         <div className="mb-3"> 
             <textarea className="form-control" id="myTextBox" onChange={handleOnChange} value={text} rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleOnClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handletoUpperCase}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handletoLowerCase}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleFirstLetterCapital}>First letter capital</button>
+        <button className="btn btn-primary mx-1" onClick={handleClearText}>Clear text</button>
      </div>
+      <div className="container my-3">
+        <h3>Your text summry</h3>
+        <p>{text.split(' ').length} Word and {text.length} Characters</p>
+        <p>{0.008 * text.split(' ').length} Minutes to read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+     </> 
+     
   )
 }
